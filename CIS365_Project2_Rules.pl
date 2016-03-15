15b263f38d9c7ce8c185e23fc4d71a623a6c6ea9
John Tunisi <John Tunisi>
2016-03-15 17:14:34 -0400

    Finished last query

--- CIS365_Project2_Rules.pl
+++ CIS365_Project2_Rules.pl
@@ -1,3 +1,11 @@
+/*----------------------------------------------------------------------
+ * CIS 365 Project 2: GVSU Registrar's Office Option #1
+ * John Tunisi and Adam Terwillier
+ * 17 March 2016
+ *----------------------------------------------------------------------*/
+
+
+/*----- Facts -----*/
 course('467', 'CS Project', '10:00 am', ' 10:50 am', 'MWF', 'MAK B1118', 'Dr. Engelsma', 'CS').
 course('463', 'IS Project', '2:00 pm', ' 2:50 pm', 'MWF', 'MAK D2123', 'Mr. Lange', 'IS').
 course('460', 'MIS', '10:00 am', ' 11:15 am', 'TR', 'MAK B1116', 'Dr. P. Leidig', 'IS').
@@ -22,9 +30,6 @@ course('330', 'Systems Analysis and Design', '9:00 am', ' 9:50 am', 'MWF', 'MAK 
 course('661', 'Medical and BioInformatics', '6:00 pm', ' 8:50 pm', 'T', 'EC 612', 'Dr. J. Leidig', 'CIS').
 course('671', 'Information Visualization', '6:00 pm', ' 8:50 pm', 'R', 'EC 612', 'Dr. J. Leidig', 'CIS').
 course('691', 'MBI Capstone', '6:00 pm', ' 8:50 pm', 'M', 'EC 612', 'Dr. J. Leidig', 'CIS').
-course('Error', 'Error', '6:00 pm', ' 8:50 pm', 'M', 'EC 612', 'Dr. J. Leidig', 'CIS').
-
-
 
 professor('Dr. Engelsma').
 professor('Mr. Lange').
@@ -61,41 +66,50 @@ courseTaken('Kara Thrace' ,'365' ,'AI').
 courseTaken('Gaius Baltar' ,'463' ,'IS Project').
 courseTaken('Gaius Baltar' ,'460' ,'MIS').
 
+/*----- Rule Combining -----*/
 taughtBy(X, C) :- course(C, _, _, _, _, _, X, _), professor(X).
 teachesAtTime(P, S, T, D) :- course(_, S, T, _, D, _, P, _).
 teachAtSameTime(X, Y, T, D) :- course(_, _, T, _, D, _, X, _), course(_, _, T, _, D, _, Y, _).
 haveSameCourse(A, B, C) :- courseTaken(A, C, _), courseTaken(B, C, _).
 inCourse(A, C) :- student(A), courseTaken(A, B, _), course(B, _, _, _, _, _, _, C).
-scheduleConflict(P, X) :- course(A, _, X, _, D, _, P, _), course(B, _, X, _, D, _, P, _), A \= B.
+scheduleConflict(P, X, D) :- course(A, _, X, _, D, _, P, _), course(B, _, X, _, D, _, P, _), A \= B.
+locationConflict(L, T, D) :- course(A, _, T, _, D, L, _, _), course(B, _, T, _, D, L, _, _), A \= B.
 
+/*----- Goal -----*/
 print_solution :-
 write('What does Dr. J. Leidig teach?'), nl,
 findall(C, taughtBy('Dr. J. Leidig', C), Query1),
 write(Query1), nl, nl,
 write('Does Dr. J. Leidig teach Database?'), nl,
-(taughtBy('Dr. J. Leidig', '353') -> Z = true ; Z = false),
-write(Z), nl, nl,
-write('What is Dr. J. Leidigs schedule'), nl,
-findall((S, E, D), course(_, _, S, E, D, _, 'Dr. J. Leidig', _), Query2),
+(taughtBy('Dr. J. Leidig', '353') -> Query2 = true ; Query2 = false),
 write(Query2), nl, nl,
-write('Who is scheduled to teach what subject on TTH, 10am?'), nl,
-findall((P, X), teachesAtTime(P, X, '10:00 am', 'TR'), Query3),
+write('What is Dr. J. Leidigs schedule'), nl,
+findall((S, E, D), course(_, _, S, E, D, _, 'Dr. J. Leidig', _), Query3),
 write(Query3), nl, nl,
-write('When do Dr. J. Leidig and Dr. El-Said teach at the same time?'), nl,
-setof((W, Y), teachAtSameTime('Dr. J. Leidig', 'Dr. El-Said', W, Y), Query4),
+write('Who is scheduled to teach what subject on TTH, 10am?'), nl,
+findall((P, X), teachesAtTime(P, X, '10:00 am', 'TR'), Query4),
 write(Query4), nl, nl,
-write('Who teaches at the same time as Dr. J. Leidig?'), nl,
-findall(A, teachAtSameTime('Dr. J. Leidig', A, _, _), Query5),
+write('When do Dr. J. Leidig and Dr. El-Said teach at the same time?'), nl,
+setof((W, Y), teachAtSameTime('Dr. J. Leidig', 'Dr. El-Said', W, Y), Query5),
 write(Query5), nl, nl,
-write('What courses do Jim and Pam have in common?'), nl,
-findall(B, haveSameCourse('Jim', 'Pam', B), Query6),
+write('Who teaches at the same time as Dr. J. Leidig?'), nl,
+findall(A, teachAtSameTime('Dr. J. Leidig', A, _, _), Query6),
 write(Query6), nl, nl,
-write('Who is taking CS Courses?'), nl,
-setof(F, inCourse(F, 'CS'), Query7),
+write('What courses do Jim and Pam have in common?'), nl,
+findall(B, haveSameCourse('Jim', 'Pam', B), Query7),
 write(Query7), nl, nl,
-write('What types of courses are Gaius Baltar taking?'), nl,
-setof(G, inCourse('Gaius Baltar', G), Query8),
+write('Who is taking CS Courses?'), nl,
+setof(F, inCourse(F, 'CS'), Query8),
 write(Query8), nl, nl,
+write('What types of courses are Gaius Baltar taking?'), nl,
+setof(G, inCourse('Gaius Baltar', G), Query9),
+write(Query9), nl, nl,
 write('Are there any scheduling conflicts of professors or locations?'), nl,
-findall(J, scheduleConflict('Dr. J. Leidig', J), Query9),
-write(Query9), nl, nl.
+write('Schedule Conflicts:'), nl,
+setof((J, K, L), scheduleConflict(J, K, L), Query10),
+write(Query10), nl,
+write('Location Conflicts:'), nl,
+setof((M, N, Q), locationConflict(M, N, Q), Query11),
+write(Query11), nl.
+
+?- print_solution.
