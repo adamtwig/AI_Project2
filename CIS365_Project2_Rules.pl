f7d1fd8ca7dfb0749ad3590d036c6fd4a9b06424
Adam Terwilliger <adamtwig@hotmail.com>
2016-03-17 01:57:24 -0400

    fixed question 6, added comments, added design doc

--- CIS365_Project2_Rules.pl
+++ CIS365_Project2_Rules.pl
@@ -1,6 +1,6 @@
 /*----------------------------------------------------------------------
  * CIS 365 Project 2: GVSU Registrar's Office Option #1
- * John Tunisi and Adam Terwillier
+ * John Tunisi and Adam Terwilliger
  * 17 March 2016
  *----------------------------------------------------------------------*/
 
@@ -67,44 +67,59 @@ courseTaken('Gaius Baltar' ,'463' ,'IS Project').
 courseTaken('Gaius Baltar' ,'460' ,'MIS').
 
 /*----- Rule Combining -----*/
+
+/*- List all courses C for professor X -*/
 taughtBy(X, C) :- course(C, _, _, _, _, _, X, _), professor(X).
+
+/*- Find what courses S are taught at time T and day D by professor P -*/
 teachesAtTime(P, S, T, D) :- course(_, S, T, _, D, _, P, _).
-teachAtSameTime(X, Y, T, D) :- course(_, _, T, _, D, _, X, _), course(_, _, T, _, D, _, Y, _).
+
+/*- Find what professors Y teach on the same time T and day D as professor X -*/
+teachAtSameTime(X, Y, T, D) :- course(_, _, T, _, D, _, X, _), course(_, _, T, _, D, _, Y, _), X \= Y.
+
+/*- Find what students A and B are taking the same course C -*/
 haveSameCourse(A, B, C) :- courseTaken(A, C, _), courseTaken(B, C, _).
+
+/*- Find what type of course C student A is taking -*/
 inCourse(A, C) :- student(A), courseTaken(A, B, _), course(B, _, _, _, _, _, _, C).
-scheduleConflict(P, X, D) :- course(A, _, X, _, D, _, P, _), course(B, _, X, _, D, _, P, _), A \= B.
+
+/*- Find what professor P is scheduled to be in time T and day D for different courses A and B -*/
+scheduleConflict(P, T, D) :- course(A, _, T, _, D, _, P, _), course(B, _, T, _, D, _, P, _), A \= B.
+
+/*- Find what location L is scheduled to be in time T and day D for different courses A and B -*/
 locationConflict(L, T, D) :- course(A, _, T, _, D, L, _, _), course(B, _, T, _, D, L, _, _), A \= B.
 
 /*----- Goal -----*/
 print_solution :-
-write('What does Dr. J. Leidig teach?'), nl,
+write('1. What does Dr. J. Leidig teach?'), nl,
 findall(C, taughtBy('Dr. J. Leidig', C), Query1),
 write(Query1), nl, nl,
-write('Does Dr. J. Leidig teach Database?'), nl,
+write('2. Does Dr. J. Leidig teach Database?'), nl,
 (taughtBy('Dr. J. Leidig', '353') -> Query2 = true ; Query2 = false),
 write(Query2), nl, nl,
-write('What is Dr. J. Leidigs schedule'), nl,
+write("3. What is Dr. J. Leidig's schedule?"), nl,
 findall((S, E, D), course(_, _, S, E, D, _, 'Dr. J. Leidig', _), Query3),
 write(Query3), nl, nl,
-write('Who is scheduled to teach what subject on TTH, 10am?'), nl,
+write('4. Who is scheduled to teach what subject on TR, 10am?'), nl,
 findall((P, X), teachesAtTime(P, X, '10:00 am', 'TR'), Query4),
 write(Query4), nl, nl,
-write('When do Dr. J. Leidig and Dr. El-Said teach at the same time?'), nl,
+write('5. When do Dr. J. Leidig and Dr. El-Said teach at the same time?'), nl,
 setof((W, Y), teachAtSameTime('Dr. J. Leidig', 'Dr. El-Said', W, Y), Query5),
 write(Query5), nl, nl,
-write('Who teaches at the same time as Dr. J. Leidig?'), nl,
+write('6. Who teaches at the same time as Dr. J. Leidig?'), nl,
 findall(A, teachAtSameTime('Dr. J. Leidig', A, _, _), Query6),
+%findall(A, allTeachAtSameTime('Dr. J. Leidig', A, _, _), Query6),
 write(Query6), nl, nl,
-write('What courses do Jim and Pam have in common?'), nl,
+write('7. What courses do Jim and Pam have in common?'), nl,
 findall(B, haveSameCourse('Jim', 'Pam', B), Query7),
 write(Query7), nl, nl,
-write('Who is taking CS Courses?'), nl,
+write('8. Who is taking CS Courses?'), nl,
 setof(F, inCourse(F, 'CS'), Query8),
 write(Query8), nl, nl,
-write('What types of courses are Gaius Baltar taking?'), nl,
+write('9. What types of courses are Gaius Baltar taking?'), nl,
 setof(G, inCourse('Gaius Baltar', G), Query9),
 write(Query9), nl, nl,
-write('Are there any scheduling conflicts of professors or locations?'), nl,
+write('10. Are there any scheduling conflicts of professors or locations?'), nl,
 write('Schedule Conflicts:'), nl,
 setof((J, K, L), scheduleConflict(J, K, L), Query10),
 write(Query10), nl,
